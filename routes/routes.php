8de4668ebfb54b262f02179eaef9d7c41de42cf9
Juan Litvin <jlitvin@digbang.com>
2019-08-05 15:30:29 -0300

    import PaymentController in routes

--- routes/routes.php
+++ routes/routes.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use litvinjuan\MPPayments\PaymentController;
 
 Route::get('/pay/{payable}', [PaymentController::class, 'form'])->name('form');
 Route::post('/pay/{payable}', [PaymentController::class, 'pay'])->name('pay');
