5617a34b9f0b0dbb35c8f2d4c263506d786f3fae
Juan Litvin <jlitvin@digbang.com>
2019-08-05 15:47:25 -0300

    Update PaymentController namespace in routes

--- routes/routes.php
+++ routes/routes.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use litvinjuan\MPPayments\PaymentController;
+use litvinjuan\MPPayments\Http\Controllers\PaymentController;
 
 Route::get('/pay/{payable}', [PaymentController::class, 'form'])->name('form');
 Route::post('/pay/{payable}', [PaymentController::class, 'pay'])->name('pay');
